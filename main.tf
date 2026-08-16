2b3b1467e78a3cb448886682747a4bb3d31fc289
mayankprabhakar05-bit <mayankprabhakar91@gmail.com>
2026-08-16 17:09:57 +0530

    added resource group

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg"{
     name = "amar_rg1"
     location = "west us"
 }
+resource "azurerm_resource_group" "rg1"{
+    name = "mayank-rg1"
+    location = "west us"
+}
